4449563cc43b1742a7b8764ef8c628dd418bbb14
peerawitsave <152257688+peerawitsave@users.noreply.github.com>
2024-09-15 15:50:20 +0700

    [UPDATE]

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
 import random
+import math
 
 pygame.init()
 
 width, height = 800, 600
 MAX_SPEED = 5
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Pygame Agent')
 
@@ -36,6 +38,47 @@ class Agent:
         seeking_force = d
         self.apply_force(seeking_force.x, seeking_force.y)
 
+    def coherence(self, agents):
+        total = pygame.Vector2(0, 0)
+        count = 0
+        for agent in agents:
+            if agent != self:
+                total += agent.position
+                count += 1
+        
+        if count > 0:   
+            center_of_mass = total/count
+            d = center_of_mass - self.position
+            if d.length() > 0:
+                coherence_force = d.normalize() * 0.05
+                self.apply_force(coherence_force.x, coherence_force.y)
+
+
+    def seperation(self, agents):
+        d = pygame.Vector2(0, 0)
+        for agent in agents:
+            dist = math.sqrt((self.position.x - agent.position.x)**2 + (self.position.y - agent.position.y)**2)
+            
+            if dist < 25:
+                d += self.position - agent.position
+        
+        seperation_force = d * 0.01
+
+        self.apply_force(seperation_force.x, seperation_force.y)
+
+
+    def alignment(self, agents):
+        v = pygame.Vector2(0, 0)
+        for agent in agents:
+            if agent != self:
+                v += agent.velocity
+
+        v /= len(agents) - 1
+        alignment_f = v * 0.1
+        self.apply_force(alignment_f.x, alignment_f.y)
+
+
+
     def draw(self, screen):
         pygame.draw.circle(screen, RED, (int(self.position.x), int(self.position.y)), 10)
 
@@ -54,12 +97,15 @@ while running:
 
     screen.fill(BLACK)
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+    #mouse_x, mouse_y = pygame.mouse.get_pos()
 
     for agent in agents:
-        agent.seek(mouse_x, mouse_y)
+        #agent.seek(mouse_x, mouse_y)
+        #agent.alignment(agents)
+        agent.coherence(agents)
         agent.update()
         agent.draw(screen)
+        agent.seperation(agents)
 
     pygame.display.flip()
     clock.tick(60)
